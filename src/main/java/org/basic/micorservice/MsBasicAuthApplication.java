d713e90bca1e046fd9a2582341e7f742396f08dc
sdrusho <shamsud.rusho@polysurance.com>
2025-09-08 13:58:42 +0600

    first commit

---
+++ src/main/java/org/basic/micorservice/MsBasicAuthApplication.java
@@ -0,0 +1,13 @@
+package org.basic.micorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBasicAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsBasicAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/basic/micorservice/MsBasicAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.basic.micorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBasicAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
